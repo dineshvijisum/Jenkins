a88a1199632784797255a24ce51a0f3c8a483115
dineshvijisum <72882573+dineshvijisum@users.noreply.github.com>
2024-12-19 22:31:41 +0530

    Update JenkinsSample.java

--- JenkinsSample.java
+++ JenkinsSample.java
@@ -2,7 +2,7 @@ public class JenkinsSample{
   public static void main(String[] arg){
     System.out.println("Hehgghllow");
     System.out.println("Good");
-    System.out.println("how to dogffg doing googhnnd");
+    System.out.println("how to dogffg doingvgvg googhnnd");
     System.out.println("how to do doing googhnnd");
     System.out.println("Googfgffgd");
   }
